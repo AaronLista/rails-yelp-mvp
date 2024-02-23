f8b82b0301463606784cb8ac3ad61f17f7396ead
Aaron Lista <aarondavidlista@gmail.com>
2024-02-23 14:53:02 -0400

    ejercicio hecho

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+categories = %w[chinese italian japanese french belgian]
+
+puts 'creating 10 restaurants'
+12.times do
+  res = Restaurant.new(
+    name: Faker::Coffee.blend_name,
+    address: Faker::Coffee.origin,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: categories.sample
+  )
+  res.save
+  puts "#{res.name} created"
+  puts 'creating 5 reviews for this restaurants'
+  5.times do
+    res.reviews.create(
+      rating: (0..5).to_a.sample,
+      content: Faker::Lorem.paragraph
+    )
+  end
+end
+puts 'all restaurants created'
